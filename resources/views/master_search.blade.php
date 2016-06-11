2d5a32032e4ac00c65de7d15e695596b7a2019fc
Max Schorradt <schorradt@publicplan.de>
2016-06-12 00:49:51 +0200

    search fix ihr noobs

---
+++ resources/views/master_search.blade.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Schul-O-Mat | Master</title>
+
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css">
+    <link rel="stylesheet" href="/sass/app.css">
+
+    <!-- Compiled and minified JavaScript -->
+    <script type="text/javascript" src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</head>
+
+<body>
+
+    <nav>
+        <div class="nav-wrapper blue">
+            <a href="#!" class="brand-logo">Schul'O'Mat</a>
+            <form action="/schulen/search" class="form header-search-wrapper hide-on-med-and-down" method="get">
+                <!-- Search Url: /schulen/search/{key} -->
+                <i class="material-icons active">search</i>
+                <input name="searchword" class="header-search-input z-depth-2" placeholder="Suche" type="text" onChange="this.form.submit();">
+
+            </form>
+            <ul class="right">
+                <li><a href=""><i class="material-icons dropdown-button" data-activates='dropdown'>more_vert</i></a></li>
+            </ul>
+            <ul id='dropdown' class='dropdown-content text-blue'>
+                <li><a href="#!">Login</a></li>
+                <li><a href="#!">Register</a></li>
+                <li class="divider"></li>
+                <li><a href="#!">Swag</a></li>
+            </ul>
+            <ul class="left hide-on-med-and-down">
+                <li><a href="/"><i class="material-icons">arrow_back</i></a></li>
+            </ul>
+
+        </div>
+    </nav>
+    <div class="row">
+        <div class="col s12">
+            <ul class="collection">
+                @foreach ($data as $d)
+                <li class="collection-item avatar">
+                    <i class="material-icons circle blue">school</i>
+                    <span class="title">{{$d->schulbez1}}</span>
+                    <p>{{$d->schulbez2}}
+                        <br> {{$d->schulbez3}}
+                        <br> {{$d->kurzbez}}
+                    </p>
+                    <a href="/schule/{{ $d->schulnr }}" class="secondary-content"><i class="blue-text material-icons">arrow_forward</i></a>
+                </li>
+                @endforeach
+            </ul>
+        </div>
+    </div>
+</body>
+
+</html>
