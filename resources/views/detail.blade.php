d6d561023c039234cb4e49d9dc349de7c78cf355
Max Schorradt <schorradt@publicplan.de>
2016-06-11 20:03:27 +0200

    test

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -30,7 +30,7 @@
                 <li><a href="#!">Swag</a></li>
             </ul>
             <ul class="left hide-on-med-and-down">
-                <li><a href="master.html"><i class="material-icons">arrow_back</i></a></li>
+                <li><a href="/schulen"><i class="material-icons">arrow_back</i></a></li>
             </ul>
 
         </div>
