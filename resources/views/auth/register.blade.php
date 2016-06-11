1762b036cfe2b236a2812b56d779bfddaad88149
Luca <luca@luca-elena.de>
2016-06-11 18:17:51 +0200

    Yo!

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -63,7 +63,7 @@
 
                     <div class="card-action">
                         <button type="submit" class="green btn btn-primary"><i class="material-icons right">vpn_key</i>Registrieren</button>
-                        <a class="btn green" href="/"></a>
+                        <a class="btn green" href="/">Go back home</a>
                     </div>
             </form>
             </div>
