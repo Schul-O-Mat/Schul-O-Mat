36459aeceaadba561fc987d49c27b4b014a0a355
Max Schorradt <schorradt@publicplan.de>
2016-06-11 11:53:37 +0200

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,10 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
+                <div class="panel-heading">Willkommen ...</div>
 
                 <div class="panel-body">
-                    Your Application's Landing Page.
+                    ... auf der Schul-O-Mat Homepage!
                 </div>
             </div>
         </div>
