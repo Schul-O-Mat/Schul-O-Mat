af1b3334930cd485961271ef0f4707aeb6f440e2
ausDensk <sokato99@gmail.com>
2016-06-11 17:15:14 +0200

    Map funzt!

--- public/js/bin/createMap.js
+++ public/js/bin/createMap.js
@@ -1,9 +1,2 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
-
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
+var map = L.map('map').setView([6, 51], 13);
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
