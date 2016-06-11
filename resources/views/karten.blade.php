dda3da73e1163797108ddd8b122769672d6558e0
Leo Maroni <maronileo@gmail.com>
2016-06-11 22:17:52 +0200

    add gktolatlong

--- resources/views/karten.blade.php
+++ resources/views/karten.blade.php
@@ -13,7 +13,9 @@
     </script>
 </head>
 <body>
-<input type="text" name="GeoDezRight_TXT" size="20" tabindex="6" style="text-align: center"><br>
-<input type="text" name="GeoDezHeight_TXT" size="20" tabindex="7" style="text-align: center"><br>
+<form name="CalcForm">
+    <input type="text" name="GeoDezRight_TXT" size="20" tabindex="6" style="text-align: center"><br>
+    <input type="text" name="GeoDezHeight_TXT" size="20" tabindex="7" style="text-align: center"><br>
+</form>
 </body>
 </html>
