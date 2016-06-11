f2318f7570a92d171b39d1252e546954490a7dbc
Julian Baumann <julianbaumann98@gmail.com>
2016-06-11 14:17:55 +0200

    Migrations for all tables

--- database/migrations/2016_06_11_101121_schule.php
+++ database/migrations/2016_06_11_101121_schule.php
@@ -27,6 +27,57 @@ class Schule extends Migration
             $table->smallInteger("lehrerZahl");
             $table->timestamps();
         });
+
+        Schema::create('schulkontakt', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text("homepage");
+            $table->text("mail");
+            $table->text("telefonnr");
+            $table->text("faxnr");
+            $table->timestamps();
+        });
+        Schema::create('schulbezeichnung', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text("schulbez1");
+            $table->text("schulbez2");
+            $table->text("schulbez3");
+            $table->text("kurzbez");
+            $table->timestamps();
+        });
+        Schema::create('schuladresse', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text("plz");
+            $table->text("ort");
+            $table->text("strasse");
+            $table->integer("lat");
+            $table->integer("long");
+            $table->timestamps();
+        });
+        Schema::create('bewertungen', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer("userID");
+            $table->tinyInteger("bewertung1");
+            $table->tinyInteger("bewertung2");
+            $table->tinyInteger("bewertung3");
+            $table->tinyInteger("bewertung4");
+            $table->tinyInteger("bewertung5");
+            $table->tinyInteger("bewertung6");
+            $table->tinyInteger("bewertung7");
+            $table->text("freitext");
+            $table->timestamps();
+        });
+        Schema::create('keywords', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text("bezeichnung");
+            $table->timestamps();
+        });
+        Schema::create('key_bew', function (Blueprint $table) {
+            $table->integer("bewertungID");
+            $table->integer("keywordID");
+            $table->boolean("positiv");
+            $table->timestamps();
+            $table->index(["bewertungID", "keywordID"]);
+        });
     }
 
     /**
@@ -37,5 +88,11 @@ class Schule extends Migration
     public function down()
     {
         Schema::drop('schulen');
+        Schema::drop('schulkontakt');
+        Schema::drop('schulbezeichnung');
+        Schema::drop('schuladresse');
+        Schema::drop('bewertungen');
+        Schema::drop('keywords');
+        Schema::drop('key_bew');
     }
 }
