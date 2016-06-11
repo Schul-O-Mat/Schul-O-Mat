812e11b6caa0d60d2334d5a273771bc00479313d
Julian Baumann <julianbaumann98@gmail.com>
2016-06-11 14:56:18 +0200

    test model

--- app/schulen.php
+++ app/schulen.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class schulen extends Model
 {
     protected $table = "schulen";
+    protected $primaryKey = "schulnr";
     public function kontakt()
     {
         
