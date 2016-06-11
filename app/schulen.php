15efb45634bca1509219b88939d8a3e022e5cce3
Julian Baumann <julianbaumann98@gmail.com>
2016-06-11 14:43:04 +0200

    model test

--- app/schulen.php
+++ app/schulen.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class schulen extends Model
 {
-    //
+    public function kontakt()
+    {
+        return $this->hasOne('App\schulkontakt', "id", "fkadresse");
+    }
 }
