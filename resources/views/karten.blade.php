c7a21b364855f60dcec0a56f901bb1bd0c55207b
Max Schorradt <schorradt@publicplan.de>
2016-06-12 14:51:01 +0200

    test

--- resources/views/karten.blade.php
+++ resources/views/karten.blade.php
@@ -9,7 +9,7 @@
         //var GKHeight = parseInt({{$hochwert}});
         //Umrechnung
         //var newcoords = gk2geo(GKRight, GKHeight);
-        console.log(gk2geo(5698296, 2571682))
+        console.log(gk2geo(2571682,5698296));
 
 function gk2geo(rw, hw)
 {
