39847a4e86eece350f5b71b7cd19a9d06d84d82b
ausDensk <sokato99@gmail.com>
2016-06-12 14:45:45 +0200

    ME?

--- resources/views/karten.blade.php
+++ resources/views/karten.blade.php
@@ -9,7 +9,7 @@
         var GKHeight = parseInt({{$hochwert}});
         //Umrechnung
         var newcoords = gk2geo(GKRight, GKHeight);
-        console.log(gk2geo(51, 6))
+        console.log(gk2geo(56982960, 25716820))
         
 
 function gk2geo(rw, hw)
