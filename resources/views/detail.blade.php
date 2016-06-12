c7d29fe86f17682ae018280aed2b6d80b76d9d8d
Luca <luca@luca-elena.de>
2016-06-12 09:50:15 +0200

    Dropdown on Navbar fixed

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -21,7 +21,7 @@
             <a href="/" class="brand-logo">Schul'O'Mat</a>
             <ul class="right hide-on-med-and-down">
                 <li><a href="/schulen/"><i class="material-icons">search</i></a></li>
-                <li><a href=""><i class="material-icons dropdown-button" data-activates='dropdown'>more_vert</i></a></li>
+                <li><a href="" class="dropdown-button" data-activates='dropdown'><i class="material-icons" >more_vert</i></a></li>
             </ul>
             <ul id='dropdown' class='dropdown-content text-blue'>
                 @if(Auth::guest())
@@ -146,8 +146,7 @@
                             </thead>
 
                             <tbody>
-                              @if(isset($keywords))
-                                @foreach($keywords as $k=>list($pos, $neg))
+                                @if(isset($keywords)) @foreach($keywords as $k=>list($pos, $neg))
                                 <tr>
                                     <td>
                                         <div class="chip green">{{$pos}}</div>
@@ -157,8 +156,7 @@
                                         <div class="chip red">{{$neg}}</div>
                                     </td>
                                 </tr>
-                                @endforeach
-                              @endif
+                                @endforeach @endif
                             </tbody>
                         </table>
                     </div>
