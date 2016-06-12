330003dd5d81a23e66547ed336606c804dd63ec7
Julian Baumann <julianbaumann98@gmail.com>
2016-06-12 09:57:46 +0200

    bugfix favicon

--- resources/views/front.blade.php
+++ resources/views/front.blade.php
@@ -11,7 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta charset="utf8">
     <title>Schul-O-Mat</title>
-    <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
 </head>
 
 <body>
