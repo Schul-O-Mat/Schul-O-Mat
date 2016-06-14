5607f0f3e19a765bd885195245212882bd5be4ea
Maximilian Schorradt <schorradt@publicplan.de>
2016-06-14 22:01:13 +0200

    fixed wrong merge conflict

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,60 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <title>Schul-O-Mat | Master</title>
-
-    <!--Import Google Icon Font-->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
-    <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css">
-    <link rel="stylesheet" href="/sass/app.css">
-
-    <!-- Compiled and minified JavaScript -->
-    <script type="text/javascript" src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
-    <script src="http://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
-    <script>
-        $(document).ready(function(){
-            $('.collapsible').collapsible({
-                accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
-            });
-        });
-    </script>
-
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
-</head>
-
-<body>
-
-    <nav>
-        <div class="nav-wrapper blue">
-            <a href="/" style="" class="brand-logo"> <img src="/img/logo.png" class="schulomaticon"></a>
-            <form action="/schulen/search" class="form header-search-wrapper hide-on-med-and-down" method="get">
-                <!-- Search Url: /schulen/search/{key} -->
-                <i class="material-icons active">search</i>
-                <input name="searchword" class="header-search-input z-depth-2" placeholder="Suche" type="text" onChange="this.form.submit();">
-            </form>
-            <ul class="right">
-                <li><a href="" class="dropdown-button" data-activates='dropdown'><i class="material-icons" >more_vert</i></a></li>
-            </ul>
-            <ul id='dropdown' class='dropdown-content text-blue'>
-                @if(Auth::guest())
-                <li><a href="/login">Login</a></li>
-                <li><a href="/register">Register</a></li>
-                @else
-                <li><a href="#">Willkommen {{Auth::user()->name}}!</a></li>
-                <li><a href="/logout">Logout</a></li>
-                @endif
-            </ul>
-            <ul class="left hide-on-med-and-down">
-                <li><a href="/"><i class="material-icons">arrow_back</i></a></li>
-            </ul>
-
-        </div>
-    </nav>
+@extends("layouts.app")
+@section("main")
     <ul class="collapsible" data-collapsible="expandable">
         <li>
             <div class="collapsible-header"><i class="material-icons">filter_list</i>Filtern</div>
@@ -207,5 +152,4 @@
         <li class="chevron_right waves-effect disabled"><a href="#"><i class="material-icons">chevron_right</i></a></li>
       @endif
   </ul>
-</body>
-</html>
+@endsection
