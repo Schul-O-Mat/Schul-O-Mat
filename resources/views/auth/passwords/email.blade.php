22db1851940e5466af94d2add9ec4108ef0120fd
labcode-de <leo@labcode.de>
2017-02-24 11:08:22 +0100

    Redesigned reset-email site

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,62 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/html">
 
-<!-- Main Content -->
-@section('main')
-<div class="container">
+<head>
+    <!--Import Google Icon Font-->
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/css/materialize.min.css">
+
+    <!--    <link rel="stylesheet" href="../assets/sass/app.css">-->
+    <link rel="stylesheet" href="/sass/login.css">
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta charset="utf8">
+    <title>Schul-O-Mat</title>
+</head>
+
+<body>
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
+        <div class="input-cart col s12 m10 push-m1 z-depth-2 grey lighten-5">
+            <div class="col s12 m5 login">
+                <h4 class="center">Passwort zur&uuml;cksetzen</h4>
+                <br>
+                <form action="{{ url('/password/email') }}" method="post" autocomplete="off">
+                    {{ csrf_field() }}
+                    <div class="row">
+                        <div class="input-field{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label for="email">E-Mail Adresse</label>
+                            <input type="text" id="email" name="email" class="validate" required="required" value="{{ old('email') }}">
+                            @if ($errors->has('email'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('email') }}</strong>
+                                </span>
+                            @endif
                         </div>
-                    @endif
-
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/email') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-envelope"></i> Send Password Reset Link
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    </div>
+                    <div class="row center">
+                        <button type="submit" class="btn waves-effect waves-light blue">Zur&uuml;cksetzen</button>
+                    </div>
+                </form>
+            </div>
+            <!-- Signup form -->
+            <div class="col s12 m7 signup">
+                <div class="signup-toggle center" >
+                    <h4 class="center">Passwort wieder eingefallen? <a href="{{action("Auth\AuthController@getLogin")}}">Anmelden</a></h4>
+                </div>
+            </div>
+            <div class="col s12">
+                <br>
+                <div class="legal center">
+                </div>
+                <div class="legal center">
+                    <div class="col s12">
+                        <p class="center grey-text" style="font-size: 14px;">Made at Jugend Hackt West 2016 by awesome people</p>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-@endsection
+</body>
+
+</html>
