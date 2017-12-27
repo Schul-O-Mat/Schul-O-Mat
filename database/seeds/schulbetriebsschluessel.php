bfba8b07cb75636ab711e5a3836f68371e33cc67
labcode-de <leo@labcode.de>
2017-12-27 18:48:51 +0100

    schulenseeder

--- database/seeds/schulbetriebsschluessel.php
+++ database/seeds/schulbetriebsschluessel.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Seeder;
 
 class schulbetriebsschluessel extends Seeder
 {
+	// TODO: LÖSCHEN mit ABHÄNGIGKEITEN
     /**
      * Run the database seeds.
      *
@@ -11,17 +12,5 @@ class schulbetriebsschluessel extends Seeder
      */
     public function run()
     {
-        DB::table("key_schulbetriebsschluessel")->truncate();
-        $schuldaten = explode("\n", file_get_contents("https://www.schulministerium.nrw.de/BiPo/OpenData/Schuldaten/key_schulbetriebsschluessel.csv"));
-        foreach($schuldaten as $k => $v){
-          $v = str_replace('"', "", $v);
-          if($k>1&&$k<count($schuldaten)-1){
-            $v = explode(";", $v);
-            DB::table("key_schulbetriebsschluessel")->insert([
-              "id" => $v[0],
-              "Schulbetrieb" => $v[1]
-            ]);
-          }
-        }
     }
 }
--- database/seeds/schulenseeder.php
+++ database/seeds/schulenseeder.php
@@ -12,47 +12,31 @@ class schulenseeder extends Seeder
     public function run()
     {
       DB::table("schuldetails")->truncate();
+      DB::table("schulen")->truncate();
       $schuldaten = explode("\n", file_get_contents("https://www.schulministerium.nrw.de/BiPo/OpenData/Schuldaten/schuldaten.csv"));
       foreach($schuldaten as $k => $v){
         $v = str_replace('"', "", $v);
         if($k>1&&$k<count($schuldaten)-1){
-          $test = explode(";",$v);
-
+          $parsedData = explode(";",$v);
+	      $id_schuldetails = DB::getPdo()->lastInsertId();
           DB::table("schuldetails")->insert(
-            [ "plz" => $test[6],
-              "ort" => $test[7],
-              "strasse" => $test[8],
-              "rw" => $test[18],
-              "hw" => $test[19]]
+            [ "plz" => $parsedData[6],
+              "ort" => $parsedData[7],
+              "strasse" => $parsedData[8],
+              "homepage" => $parsedData[14],
+              "mail" => $parsedData[13],
+              "telnr" => $parsedData[9]."/".$pgarsedData[10],
+              "faxnr" => $parsedData[11]."/".$parsedData[12]
+	        ]
           );
-          $id_schulbezeichnung = DB::getPdo()->lastInsertId();
-          DB::table("schulbezeichnung")->insert(
-            [ "schulbez1" => $test[2],
-              "schulbez2" => $test[3],
-              "schulbez3" => $test[4],
-              "kurzbez"   => $test[5]]
-          );
-          $id_schulkontakt = DB::getPdo()->lastInsertId();
-          DB::table("schulkontakt")->insert(
-            [ "homepage" => $test[14],
-              "mail" => $test[13],
-              "telefonnr" => $test[9]."/".$test[10],
-              "faxnr" => $test[11]."/".$test[12]]
-          );
-          $id_schuladresse = DB::getPdo()->lastInsertId();
+          //TODO: Insert BundeslandID from bundeslandSeeder
           DB::table("schulen")->insert(
-            [ "schulnr" => $test[0],
-              "schulform" => $test[1],
-              "fkkontakt" => $id_schulkontakt,
-              "fkadresse" => $id_schuladresse,
-              "fkbezeichnungen" => $id_schulbezeichnung,
-              "rechtsform" => $test[15],
-              "traegernr" => $test[16],
-              "gemeindeschluessel" => $test[17],
-              "schulbetriebsschluessel" => $test[20],
-              "datum" => $test[21],
-              "schuelerzahl" => 0,
-              "lehrerZahl" => 0]
+            [
+              "schuldetailID" => $id_schuldetails,
+              "schulformId" => $parsedData[1],
+              "bezeichnung" => $parsedData[2] + $parsedData[3] + $parsedData[4],
+              "bezeichnung_kurz" => $parsedData[5]
+            ]
           );
         }
       }
